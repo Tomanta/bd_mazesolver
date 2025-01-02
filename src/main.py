d9b6bd01085fbc0172b4a9ffd9349f7c23a3e5d6
Tomanta <bzednick@gmail.com>
2025-01-02 17:40:29 -0600

    Made some data members private

--- src/main.py
+++ src/main.py
@@ -3,10 +3,10 @@ from tkinter import Tk, BOTH, Canvas
 class Window():
     def __init__(self, width, height):
         self.__root = Tk()
-        self.__root.title = "Maze Solver"
-        self.canvas = Canvas(width=width, height=height)
-        self.canvas.pack()
-        self.is_running = False
+        self.__root.title("Maze Solver")
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__is_running = False
         self.__root.protocol("WM_DELETE_WINDOW",self.close) # Attach the self.close() method to deleting the window
         
     def redraw(self):
@@ -14,14 +14,13 @@ class Window():
         self.__root.update()
 
     def wait_for_close(self):
-        self.is_running = True
-        while self.is_running:
+        self.__is_running = True
+        while self.__is_running:
             self.redraw()
+        print("window closed...")
 
     def close(self):
-        self.is_running = False
-
-        
+        self.__is_running = False
 
 def main():
     win = Window(800, 600)
