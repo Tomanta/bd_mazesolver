80511ccb9374f3a8b6b098b9102b91439619ba3c
Tomanta <bzednick@gmail.com>
2025-01-02 17:51:51 -0600

    Can now draw lines

--- src/graphics.py
+++ src/graphics.py
@@ -1,5 +1,20 @@
 from tkinter import Tk, BOTH, Canvas
 
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2
+        )
+
 class Window():
     def __init__(self, width, height):
         self.__root = Tk()
@@ -21,3 +36,6 @@ class Window():
 
     def close(self):
         self.__is_running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,25 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 def main():
     win = Window(800, 600)
+
+    p1 = Point(5,5)
+    p2 = Point(20,15)
+    l1 = Line(p1, p2)
+
+    p3 = Point(20,15)
+    p4 = Point(150,159)
+    l2 = Line(p3, p4)
+
+    p5 = Point(20,15)
+    p6 = Point(75,262)
+    l3 = Line(p5, p6)
+
+
+    win.draw_line(l1, fill_color="red")
+    win.draw_line(l2, fill_color="blue")
+    win.draw_line(l3, fill_color="green")
+
     win.wait_for_close()
 
 if __name__ == "__main__":
