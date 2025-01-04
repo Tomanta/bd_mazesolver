d0f6b987e087f2a893d8aa03562237023dd0c0c0
Tomanta <bzednick@gmail.com>
2025-01-03 20:28:02 -0600

    Added maze class

--- src/cell.py
+++ src/cell.py
@@ -1,18 +1,22 @@
 from graphics import Point, Line
 
 class Cell():
-    def __init__(self, x1: int, y1: int, x2: int, y2: int, window):
+    def __init__(self, window):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None
+        self._win = window
+    
+    def draw(self, x1: int, y1: int, x2: int, y2: int):
         self._x1 = x1
         self._y1 = y1
         self._x2 = x2
         self._y2 = y2
-        self._win = window
-    
-    def draw(self):
         top_left = Point(self._x1, self._y1)
         top_right = Point(self._x2, self._y1)
         bottom_left = Point(self._x1, self._y2)
--- src/main.py
+++ src/main.py
@@ -1,25 +1,12 @@
 from graphics import Window
-from cell import Cell
+from maze import Maze
 
 def main():
     win = Window(800, 600)
 
-    cell1 = Cell(50, 50, 100, 100, win)
-    cell1.has_bottom_wall = False
-    cell1.draw()
-
-    cell2 = Cell(50, 100, 100, 150, win)
-    cell2.has_top_wall = False
-    cell2.has_right_wall = False
-    cell2.draw()
-
-    cell3 = Cell(100, 100, 150, 150, win)
-    cell3.has_left_wall = False
-    cell3.draw()
-
-    cell1.draw_move(cell2)
-    cell2.draw_move(cell3)
-    cell3.draw_move(cell2, undo=True)
+    maze = Maze(
+        50, 50, 10, 10, 50, 50, win
+    )
 
     win.wait_for_close()
 
