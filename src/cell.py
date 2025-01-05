8a2b60501e5d77f17412a040f2e086af0dce523b
Tomanta <bzednick@gmail.com>
2025-01-04 21:00:39 -0600

    Can now remove entrance and exit of maze

--- src/cell.py
+++ src/cell.py
@@ -23,18 +23,28 @@ class Cell():
         top_right = Point(self._x2, self._y1)
         bottom_left = Point(self._x1, self._y2)
         bottom_right = Point(self._x2, self._y2)
+        
+        left_wall = Line(top_left, bottom_left)
+        top_wall = Line(top_left, top_right)
+        right_wall = Line(top_right, bottom_right)
+        bottom_wall = Line(bottom_left, bottom_right)
+
         if self.has_left_wall:
-            left_wall = Line(top_left, bottom_left)
             self._win.draw_line(left_wall, fill_color="black")
+        else:
+            self._win.draw_line(left_wall, fill_color="white")
         if self.has_top_wall:
-            top_wall = Line(top_left, top_right)
             self._win.draw_line(top_wall, fill_color="black")
+        else:
+            self._win.draw_line(top_wall, fill_color="white")
         if self.has_right_wall:
-            right_wall = Line(top_right, bottom_right)
             self._win.draw_line(right_wall, fill_color="black")
+        else:
+            self._win.draw_line(right_wall, fill_color="white")
         if self.has_bottom_wall:
-            bottom_wall = Line(bottom_left, bottom_right)
             self._win.draw_line(bottom_wall, fill_color="black")
+        else:
+            self._win.draw_line(bottom_wall, fill_color="white")
 
     def get_center(self):
         return Point(self._x1 + (self._x2 - self._x1) // 2, self._y1 + (self._y2 - self._y1) // 2)
