e4a5d9bae8dac176f6b2d2158eb2a7f81aa15b4c
Tomanta <bzednick@gmail.com>
2025-01-04 20:50:45 -0600

    Started unit tests

--- src/maze.py
+++ src/maze.py
@@ -10,7 +10,7 @@ class Maze():
             num_cols,
             cell_size_x,
             cell_size_y,
-            win
+            win = None
     ):
         self._x1 = x1
         self._y1 = y1
@@ -33,15 +33,11 @@ class Maze():
         
         for i in range(self._num_cols):
             for j in range(self._num_rows):
-                try:
-                    self._draw_cell(i, j)
-                except:
-                    print(f"Error: {i} {j}")
-                    raise Exception("Error")
+                self._draw_cell(i, j)
 
     def _draw_cell(self, i, j):
         if self._win is None:
-            raise Exception("maze._draw_cell: Window not found")
+            return
         x1 = self._x1 + i * self._cell_size_x
         y1 = self._y1 + j * self._cell_size_y
         x2 = x1 + self._cell_size_x
