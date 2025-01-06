f5587a9b51a7f2e1ca2ac8889ed83ed0ddd7f466
Tomanta <bzednick@gmail.com>
2025-01-05 19:56:21 -0600

    cleanup

--- src/graphics.py
+++ src/graphics.py
@@ -40,28 +40,3 @@ class Window():
     def draw_line(self, line, fill_color):
         line.draw(self.__canvas, fill_color)
 
-class Cell():
-    def __init__(self, x1: int, y1: int, x2: int, y2: int, window: Window):
-        self.has_left_wall = True
-        self.has_right_wall = True
-        self.has_top_wall = True
-        self.has_bottom_wall = True
-        self._x1 = x1
-        self._y1 = y1
-        self._x2 = x2
-        self._y2 = y2
-        self._win = window
-    
-    def draw(self, x1: int, y1: int, x2: int, y2: int):
-        if self.has_left_wall:
-            left_wall = Line(Point(x1, y1), Point(x1, y2))
-            self._win.draw_line(left_wall, fill_color="black")
-        if self.has_top_wall:
-            top_wall = Line(Point(x1, y1), Point(x2, y1))
-            self._win.draw_line(top_wall, fill_color="black")
-        if self.has_right_wall:
-            right_wall = Line(Point(x2, y1), Point(x2, y2))
-            self._win.draw_line(right_wall, fill_color="black")
-        if self.has_bottom_wall:
-            bottom_wall = Line(Point(x1, y2), Point(x2, y2))
-            self._win.draw_line(bottom_wall, fill_color="black")
